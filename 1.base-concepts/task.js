60243556ca76a67c202440ee85cbcada1667177b
kozlyatinka <kozlyatinka123@yandex.ru>
2022-11-26 16:14:21 +0300

    7 try

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,7 +4,7 @@ function solveEquation(a, b, c) {
   let d = Math.pow(b, 2) - 4*a*c;
   if (d < 0) {
     arr = [];
-  } else if (d == 0) {
+  } else if (d === 0) {
     arr.push(-b / 2 * a);
   } else {
     arr.push((-b + Math.sqrt(d))/(2*a));
