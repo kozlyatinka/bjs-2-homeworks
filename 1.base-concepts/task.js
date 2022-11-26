54e1c1413e886f47f1fd7d8117ccbdda199841fe
kozlyatinka <kozlyatinka123@yandex.ru>
2022-11-26 15:39:04 +0300

    Первая попытка

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let firstRoot;
+  let secondRoot;
+  let d = Math.pow(b, 2) - 4*a*c;
+  if (d < 0) {
+    arr = [];
+  } else if (d = 0) {
+    firstRoot = -b/2*a;
+    arr.push(firstRoot);
+  } else {
+    firstRoot = (-b + Math.sqrt(d))/(2*a);
+    secondRoot = (-b - Math.sqrt(d))/(2*a);
+    arr.push(firstRoot, secondRoot)
+  }
   return arr;
 }
 
