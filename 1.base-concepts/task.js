f1f862e8f4f798298c604be6df661e34b7f07b8f
kozlyatinka <kozlyatinka123@yandex.ru>
2022-11-28 20:32:43 +0300

    stcjnd task+-

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,15 +2,43 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = Math.pow(b, 2) - 4*a*c;
-  if (d < 0) {
-    arr = [];
-  } else if (d === 0) {
+  if (d === 0) {
     arr.push(-b / 2 * a);
-  } else {
+  } else if (d > 0){
     arr.push((-b + Math.sqrt(d))/(2*a));
     arr.push((-b - Math.sqrt(d))/(2*a));
-  }
+  } 
   return arr;
 }
 
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  //percent - процентная ставка
+  //contribution - сумму первоначального взноса
+  //amount - сумму кредита
+  //countMonths - срок 
+  let totalCreditSum;
+  let monthlyPayment;
+  let monthlyPercent = percent/100/12;
+  let creditBody = amount - contribution;
 
+  if (isNaN(percent)) {
+    return (`Параметр Процентная ставка содержит неправильное значение ${percent}`);
+  } 
+  if (isNaN(contribution)) {
+    return (`Параметр Процентная ставка содержит неправильное значение ${contribution}`);
+  }
+
+  if (isNaN(amount)) {
+    return (`Параметр Процентная ставка содержит неправильное значение ${amount}`);
+  }
+
+  if (isNaN(countMonths)) {
+    return (`Параметр Процентная ставка содержит неправильное значение ${countMonths}`);
+  }
+
+  monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent)**countMonths) - 1)));
+  totalCreditSum = parseFloat((monthlyPayment * countMonths).toFixed(2));
+    
+  return totalCreditSum;
+
+}
