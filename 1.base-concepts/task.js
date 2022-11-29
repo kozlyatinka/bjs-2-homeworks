064d0b6d447532cee17812e895320a4c4f3ad955
kozlyatinka <kozlyatinka123@yandex.ru>
2022-11-29 10:31:01 +0300

    last chance

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -25,15 +25,15 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
     return (`Параметр Процентная ставка содержит неправильное значение ${percent}`);
   } 
   if (isNaN(contribution)) {
-    return (`Параметр Процентная ставка содержит неправильное значение ${contribution}`);
+    return (`Параметр Сумма первоначального кредита содержит неправильное значение ${contribution}`);
   }
 
   if (isNaN(amount)) {
-    return (`Параметр Процентная ставка содержит неправильное значение ${amount}`);
+    return (`Параметр Сумма кредита содержит неправильное значение ${amount}`);
   }
 
   if (isNaN(countMonths)) {
-    return (`Параметр Процентная ставка содержит неправильное значение ${countMonths}`);
+    return (`Параметр Срок содержит неправильное значение ${countMonths}`);
   }
 
   monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent)**countMonths) - 1)));
