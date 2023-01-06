ca73b487c6d731f29dbcc9cc7a737c2112ecbfe1
kozlyatinka <kozlyatinka@bk.ru>
2023-01-06 13:19:40 +0300

    3 done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length && arr1.every((n, i) => arr2[i] === n)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => {
+    acc +=item;
+    if (index === arr.length - 1){
+        return acc / arr.length;
+    }
+    return acc;
+    }, 0)
+    return result;
 }
